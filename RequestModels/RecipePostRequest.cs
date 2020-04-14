530f074127909e0b7c1fda4c909143c5a100e7fe
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-14 12:29:01 +0200

    Add RecipePostRequest

---
+++ RequestModels/RecipePostRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipie.RequestModels
+{
+    public class RecipePostRequest
+    {
+        public string OwnerName { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
