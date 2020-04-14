d3eab0e0a999c70830a089b696f8c3bf85695fe9
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-14 12:28:12 +0200

    Add swagger

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 using Recipie.Data;
 
 namespace Recipie
@@ -30,6 +32,10 @@ namespace Recipie
             services.AddControllers();
             services.AddDbContext<RecipeContext>(
                 options => options.UseSqlServer(Configuration.GetConnectionString("RecipeConnection")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +55,15 @@ namespace Recipie
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
