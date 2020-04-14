67c5307ff3e8385cde30561b42444081cb39f3c3
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-14 13:55:28 +0200

    Add IngredientPostRequest

---
+++ RequestModels/IngredientPostRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipie.RequestModels
+{
+    public class IngredientPostRequest
+    {
+        public string OwnerName { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
