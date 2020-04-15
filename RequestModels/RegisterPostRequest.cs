707d40103596b7d4329e423637663cd07a75a9cb
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-15 13:02:10 +0200

    Add RegisterPostRequest

---
+++ RequestModels/RegisterPostRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipie.RequestModels
+{
+    public class RegisterPostRequest
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
