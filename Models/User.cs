7e045a4c0d04c2261e5afbd2608978b3d181af01
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-15 12:44:35 +0200

    Add User model

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Recipie.Domain.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Recipie.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Introduction { get; set; }
+        public string ProfilePicture { get; set; }
+        [NotMappedAttribute]
+        public List<Recipe> Recipes { get; set; }
+    }
+}
