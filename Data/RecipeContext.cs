0eb35c94ab7df8dee1727c8eb1ec0e2937a404b4
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-15 12:46:18 +0200

    Add User to DbContext

--- Data/RecipeContext.cs
+++ Data/RecipeContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Recipie.Domain.Models;
 using Recipie.Models;
 using System;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Recipie.Data
 {
-    public class RecipeContext : DbContext
+    public class RecipeContext : IdentityDbContext<User>
     {
         public RecipeContext(DbContextOptions<RecipeContext> options)
             : base(options) { }
@@ -17,9 +18,11 @@ namespace Recipie.Data
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<RecipeIngredient>()
                 .HasKey(s => new { s.IngredientId, s.RecipeId });
         }
