b1422d1b4277b121102084a388c0bd96b3949179
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-15 13:46:05 +0200

    Add Login

---
+++ RequestModels/LoginPostRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipie.RequestModels
+{
+    public class LoginPostRequest
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
