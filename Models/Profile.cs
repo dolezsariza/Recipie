06730a53b4ec0d7e7ba75d7a138a6db25005898e
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-15 13:20:49 +0200

    Add profile model

---
+++ Models/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipie.Models
+{
+    public class Profile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Introduction { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
