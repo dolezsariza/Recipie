73c8a3139015c4132742902fa05b2c8e6e163c58
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-17 14:14:42 +0200

    Add category and subcategory models

---
+++ Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using Recipie.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipie.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<SubCategory> SubCategories { get; set; }
+        public List<Recipe> Recipes { get; set; }
+        public Category() { }
+        public Category(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ Models/SubCategory.cs
@@ -0,0 +1,27 @@
+﻿using Recipie.Domain.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Recipie.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //[ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public List<Recipe> Recipes { get; set; }
+
+        public SubCategory() { }
+        public SubCategory(string name, string description, int categoryId)
+        {
+            Name = name;
+            Description = description;
+            CategoryId = categoryId;
+        }
+    }
+}
