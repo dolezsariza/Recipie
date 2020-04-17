10e19a40b6e5fb80298c6e6892626eeb60e72c59
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-17 14:53:33 +0200

    Add DBset for categories and tags

--- Data/RecipeContext.cs
+++ Data/RecipeContext.cs
@@ -19,6 +19,9 @@ namespace Recipie.Data
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
