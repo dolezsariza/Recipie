8bfc51308c4d1738727c5f139a8ecdad2b23c4f9
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-11-01 15:08:50 +0100

    Create LoginRepository

---
+++ Repositories/LoginRepository/Interfaces/ILoginRepository.cs
@@ -0,0 +1,14 @@
+﻿using Recipie.Models;
+using Recipie.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipie.Repositories.LoginRepository.Interfaces
+{
+    public interface ILoginRepository
+    {
+        Task<User> Login(LoginPostRequest login);
+    }
+}
---
+++ Repositories/LoginRepository/LoginRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Recipie.Data;
+using Recipie.Models;
+using Recipie.Repositories.LoginRepository.Interfaces;
+using Recipie.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipie.Repositories.LoginRepository
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private readonly RecipeContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public LoginRepository(RecipeContext context, UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _context = context;
+            _signInManager = signInManager;
+        }
+        public async Task<User> Login(LoginPostRequest login)
+        {
+            try
+            {
+                var user = await _userManager.FindByNameAsync(login.Username);
+                var result = await _userManager.CheckPasswordAsync(
+                    user, login.Password);
+                await _signInManager.SignInAsync(user, isPersistent: true);
+                return user;
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+    }
+}
