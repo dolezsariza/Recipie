0472b339e4e5a3a1d452208adf16ab8c0a43918e
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-11-01 14:48:30 +0100

    Create Authenticator for admin check

---
+++ Repositories/LoginRepository/Authenticator.cs
@@ -0,0 +1,30 @@
+﻿using Recipie.Data;
+using Recipie.Repositories.LoginRepository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipie.Repositories.LoginRepository
+{
+    public class Authenticator : IAuthenticator
+    {
+        private RecipeContext _context;
+        public Authenticator(RecipeContext context)
+        {
+            _context = context;
+        }
+        public bool AuthenticateUser(string userName)
+        {
+            var user = _context.Users.Where(u => u.UserName == userName).FirstOrDefault();
+            if(user != null) return (user.RoleName == "admin");
+            return false;
+        }
+
+        public bool CheckIfUserIsOwnerOfRecipe(string userName, int recipeId)
+        {
+            var recipe = _context.Recipes.Where(rec => rec.ID == recipeId).FirstOrDefault();
+            return (recipe.OwnerName == userName);
+        }
+    }
+}
---
+++ Repositories/LoginRepository/Interfaces/IAuthenticator.cs
@@ -0,0 +1,14 @@
+﻿using Recipie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipie.Repositories.LoginRepository.Interfaces
+{
+    public interface IAuthenticator
+    {
+        bool AuthenticateUser(string userName);
+        bool CheckIfUserIsOwnerOfRecipe(string userName, int recipeId);
+    }
+}
